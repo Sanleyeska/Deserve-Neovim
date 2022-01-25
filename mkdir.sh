283ae711c4e3c1221c8473f04a78372bf6db67fe
sanluci <98359074+sanluci@users.noreply.github.com>
2022-01-24 22:19:42 -0400

    mkdir.sh upload

---
+++ mkdir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Creating directories and files..."
+echo "--"
+
+mkdir ~/.config/nvim
+mkdir ~/.config/nvim/general
+mkdir ~/.config/nvim/lua
+touch ~/.config/nvim/init.vim
+touch ~/.config/nvim/init.vim
+touch ~/.config/nvim/general/settings.vim
+touch ~/.config/nvim/general/maps.vim
+touch ~/.config/nvim/general/plugins.vim
+touch ~/.config/nvim/general/plugins-cfg.vim
+touch ~/.config/nvim/general/functions.vim
+touch ~/.config/nvim/general/simple-pairs.vim
+
+echo "Done!"
